3780b77ddba55d4031671bf4a212ad568ec884e6
Tharunraj Optimus <tharunoptimus@outlook.com>
2021-10-04 17:16:34 +0530

    Added empty user variable to be used by lovefield

--- assets/js/qrCodeScanner.js
+++ assets/js/qrCodeScanner.js
@@ -1,11 +1,12 @@
-const video = document.createElement("video");
-const canvasElement = document.getElementById("qr-canvas");
-const canvas = canvasElement.getContext("2d");
+let user
+const video = document.createElement("video")
+const canvasElement = document.getElementById("qr-canvas")
+const canvas = canvasElement.getContext("2d")
 
-const qrResult = document.getElementById("qr-result");
-const outputData = document.getElementById("outputData");
-const btnScanQR = document.getElementById("btn-scan-qr");
-const loadingScreen = document.querySelector(".loadingContainer");
+const qrResult = document.getElementById("qr-result")
+const outputData = document.getElementById("outputData")
+const btnScanQR = document.getElementById("btn-scan-qr")
+const loadingScreen = document.querySelector(".loadingContainer")
 
 let scanning = false;
 
