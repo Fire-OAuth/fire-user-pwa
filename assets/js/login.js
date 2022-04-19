a1db448b8a8b3be7cc04216b7f44cdb458cd9960
tharunoptimus <tharunoptimus@outlook.com>
2022-04-19 22:38:14 +0530

    Login now uses localforage

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,79 +1,63 @@
 let fireServerURL = 'http://localhost:3003/api/users'
-let user
-let schemaBuilder = lf.schema.create("userData", 1)
-
-schemaBuilder
-    .createTable("User")
-    .addColumn("id", lf.Type.INTEGER)
-    .addColumn("_id", lf.Type.STRING)
-    .addColumn("email", lf.Type.STRING)
-    .addColumn("firstName", lf.Type.STRING)
-    .addColumn("lastName", lf.Type.STRING)
-    .addColumn("profilePic", lf.Type.STRING)
-    .addPrimaryKey(["id"], true);
-
-let userDb
-let item
-
-schemaBuilder.connect().then(function (db) {
-    userDb = db;
-    item = db.getSchema().table("User")
-})
 
 async function submitForm (type) {
 
-    let data = {}
+    try {
+        let data = {}
 
-    if(type == 'login') {
-        data = {
-            email: document.getElementById('email').value,
-            password: document.getElementById('password').value
+        if(type == 'login') {
+            data = {
+                email: document.getElementById('email').value,
+                password: document.getElementById('password').value
+            }
         }
-    }
-    else if(type == 'register') {
-        data = {
-            email: document.getElementById('email').value,
-            firstName: document.getElementById('firstName').value,
-            lastName: document.getElementById('lastName').value,
-            password: document.getElementById('password').value
+        else if(type == 'register') {
+            data = {
+                email: document.getElementById('email').value,
+                firstName: document.getElementById('firstName').value,
+                lastName: document.getElementById('lastName').value,
+                password: document.getElementById('password').value
+            }
         }
+
+        let endpoint = type == 'login' ? 'login' : 'register'
+
+        let response = await fetch(`${fireServerURL}/${endpoint}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+
+        let responseStatus
+
+        if (type == 'register') responseStatus = response.status == 201 ? true : false
+        else responseStatus = response.status == 200 ? true : false
+
+        response = await response.json();
+        console.log(response)
+        if(responseStatus){
+            await createItem(response).catch(err => console.log(err))
+    
+            document.querySelector(".loading").style.display = "flex";
+            setTimeout(() => {
+                window.location.href = '/';
+            }, 2000)
+        }
+        else {
+            document.querySelector(".errorMessage").innerHTML = response.message;
+        }
+        
+    } catch (error) {
+        console.log(error)
+        document.querySelector(".errorMessage").innerHTML = "Something Happened. Try Again Later";
     }
-
-    let endpoint = type == 'login' ? 'login' : 'register'
-
-
-
-    let response = await fetch(`${fireServerURL}/${endpoint}`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    })
-
-    let responseStatus
-
-    if (type == 'register') responseStatus = response.status == 201 ? true : false
-    else responseStatus = response.status == 200 ? true : false
-
-    response = await response.json();
-    console.log(response)
-    if(responseStatus){
-        await createItem(response).catch(err => console.log(err))
-  
-        document.querySelector(".loading").style.display = "flex";
-        setTimeout(() => {
-            window.location.href = '/';
-        }, 2000)
-    }
-    else {
-        document.querySelector(".errorMessage").innerHTML = response.message;
-    }
-
+    
     return false;
 }
 
 async function createItem(data) {
-	let row = item.createRow(data)
-	return await userDb.insertOrReplace().into(item).values([row]).exec().catch(err => console.log(err))
+    let user = localforage.setItem('user', data)
+    return user
 }
