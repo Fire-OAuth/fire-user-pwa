63d1573dcac92557711a29ece407a5db014efcc3
tharunoptimus <tharunoptimus@outlook.com>
2022-04-19 22:49:30 +0530

    Home now uses localforage

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,76 +1,22 @@
-let user
+let user = null
 
-let schemaBuilder = lf.schema.create("userData", 1)
-let transactionSchemaBuilder = lf.schema.create("transactionData", 2)
-
-schemaBuilder
-    .createTable("User")
-    .addColumn("id", lf.Type.INTEGER)
-    .addColumn("_id", lf.Type.STRING)
-    .addColumn("email", lf.Type.STRING)
-    .addColumn("firstName", lf.Type.STRING)
-    .addColumn("lastName", lf.Type.STRING)
-    .addColumn("profilePic", lf.Type.STRING)
-    .addPrimaryKey(["id"], true);
-
-transactionSchemaBuilder
-    .createTable("Transactions")
-    .addColumn("id", lf.Type.INTEGER)
-    .addColumn("token", lf.Type.STRING)
-    .addColumn("url", lf.Type.STRING)
-    .addColumn("time", lf.Type.STRING)
-    .addColumn("method", lf.Type.STRING)
-    .addPrimaryKey(["id"], true);
-
-let userDb
-let item
-
-let transactionDb
-let transaction
-
-schemaBuilder.connect().then(async (db) => {
-    userDb = db;
-    item = db.getSchema().table("User")
+async function main () {
     user = await getUserDetails()
-    if(user != undefined) {
+
+    if(user != null || user != undefined) {
         document.querySelector(".userCard").innerHTML = returnUserCard(user)
         document.querySelector(".loading").remove()
-    }
-    else {
-        // userDb.delete().from(item).exec() // Clear session or Logout Query
+    } else {
         window.location.href = "/login.html"
     }
-})
-
-async function createItem(data) {
-	let row = item.createRow(data)
-	return await userDb.insertOrReplace().into(item).values([row]).exec().catch(err => console.log(err))
 }
 
-function getUserDetails() {
-    return new Promise((resolve, reject) => {
-        let user;
-        userDb
-		.select()
-		.from(item)
-		.exec()
-        .then(res => {
-            user = res.at(-1);
-            resolve(user)
-        })
-    })
+async function getUserDetails() {
+    return await localforage.getItem('user')
 }
 
 async function getTransactions() {
-    return new Promise((resolve, reject) => {
-        transactionDb
-		.select()
-		.from(transaction)
-		.exec()
-        .then(res => {
-            resolve (res)
-        })
-    })
+    return await localforage.getItem('transactions')
 }
 
 window.addEventListener("click", (e) => {
@@ -88,21 +34,18 @@ window.addEventListener("click", (e) => {
 
 
 document.querySelector(".showPreviousSignups").addEventListener("click", async () => {
-    transactionSchemaBuilder.connect().then(async function (db) {
-        transactionDb = db
-        transaction = db.getSchema().table("Transactions")
-        
-        let data = await getTransactions()
-        let html = showListOfTransactions(data)
+    let data = await getTransactions()
+    let html = showListOfTransactions(data)
 
-        document.querySelector(".showPreviousSignups").remove()
+    document.querySelector(".showPreviousSignups").remove()
 
-        html = `<div class="heading">Previous Signups</div>` + html
+    html = `<div class="heading">Previous Signups</div>` + html
 
-        document.querySelector(".signupsContainer").innerHTML = html
-    })
+    document.querySelector(".signupsContainer").innerHTML = html
 })
 
 document.getElementById("qrAuthorize").addEventListener("click", async () => {
     window.location.href = "/qr.html"
 })
+
+main()
